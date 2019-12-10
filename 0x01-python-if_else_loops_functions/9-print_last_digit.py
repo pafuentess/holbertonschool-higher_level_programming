62bc59b14a0b6f6311428d5417c86700c2e912fa
pafuentess <pafuentess@unal.edu.co>
2019-12-10 11:27:18 -0500

    lo q faltaba

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = number % -10
+        print(-last, end='')
+        return(-last)
+    else:
+        last = number % 10
+        print(last, end='')
+        return(last)
