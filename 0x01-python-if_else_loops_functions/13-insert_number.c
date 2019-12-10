30852235c6e258b9aecc06ae36329ddf6d5514d2
pafuentess <pafuentess@unal.edu.co>
2019-12-10 14:04:57 -0500

    insert before first node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,23 +15,30 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current1, *current2;
 
 	current1 = *head;
-	current2 = current1->next;
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
 
-	while (current1)
+	if (number < current1->n)
 	{
-		if (number > current1->n && number < current2->n)
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+        	current2 = current1->next;
+		while (current1)
 		{
-			current1->next = new;
-			new->next = current2;
-			break;
+			if (number > current1->n && number < current2->n)
+			{
+				current1->next = new;
+				new->next = current2;
+				break;
+			}
+			current1 = current1->next;
+			current2 = current2->next;
 		}
-		current1 = current1->next;
-		current2 = current2->next;
 	}
 	return (new);
 }
