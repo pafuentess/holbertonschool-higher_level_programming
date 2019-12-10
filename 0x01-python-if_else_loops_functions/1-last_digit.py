04aeeb51cb458d030cb972523c60a2815a08f7ee
pafuentess <pafuentess@unal.edu.co>
2019-12-10 12:23:49 -0500

    upper case L

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number < 0:
     last = number % -10
 else:
     last = number % 10
-print("last digit of ", number, "is", last, end=' ')
+print("Last digit of ", number, "is", last, end=' ')
 if last > 5:
     print("and is greater than 5")
 elif last == 0:
