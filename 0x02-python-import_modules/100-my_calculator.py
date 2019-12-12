a43ba3e26e25515e65b0748dfaacca2ed1f3bb2d
pafuentess <pafuentess@unal.edu.co>
2019-12-12 12:12:48 -0500

    fix import

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
 
-argc = len(argv)
-if argc < 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    argc = len(argv)
+    print(argc)
+    if argc < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if (argv[2] == "+"):
+            print("{:s} + {:s} = {:d}".format(argv[1], argv[3], add(a, b)))
+            exit(0)
+        elif (argv[2] == "-"):
+            print("{:s} - {:s} = {:d}".format(argv[1], argv[3], sub(a, b)))
+            exit(0)
+        elif (argv[2] == "*"):
+            print("{:s} * {:s} = {:d}".format(argv[1], argv[3], mul(a, b)))
+            exit(0)
+        elif (argv[2] == "/"):
+            print("{:s} / {:s} = {:d}".format(argv[1], argv[3], div(a, b)))
+            exit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
-else:
-    a = int(argv[1])
-    b = int(argv[3])
-    if (argv[2] == "+"):
-        print("{:s} + {:s} = {:d}".format(argv[1], argv[3], add(a, b)))
-        exit(0)
-    elif (argv[2] == "-"):
-        print("{:s} - {:s} = {:d}".format(argv[1], argv[3], sub(a, b)))
-        exit(0)
-    elif (argv[2] == "*"):
-        print("{:s} * {:s} = {:d}".format(argv[1], argv[3], mul(a, b)))
-        exit(0)
-    elif (argv[2] == "/"):
-        print("{:s} / {:s} = {:d}".format(argv[1], argv[3], div(a, b)))
-        exit(0)
-print("Unknown operator. Available operators: +, -, * and /")
-exit(1)
