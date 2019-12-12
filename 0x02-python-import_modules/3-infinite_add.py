0afd4fe1485ca8564c62994d8e6dc5384aec09fa
pafuentess <pafuentess@unal.edu.co>
2019-12-12 12:09:11 -0500

    fix import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-argc = len(argv) - 1
-suma = 0
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) - 1
+    suma = 0
 
-if (argc == 1):
-    print(0)
-else:
-    for i in argv[1:]:
-        suma = suma + int(i)
-print(suma)
+    if (argc == 1):
+        print(0)
+    else:
+        for i in argv[1:]:
+            suma = suma + int(i)
+    print(suma)
