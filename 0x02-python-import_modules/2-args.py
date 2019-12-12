cbac5ceea8b8e85ef6e202b4d1e275c566800afd
pafuentess <pafuentess@unal.edu.co>
2019-12-12 12:05:42 -0500

    fix import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-argc = len(argv) - 1
-if (argc == 0):
-    print(argc, "arguments.")
-else:
-    if (argc == 1):
-        print(argc, "argument:")
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) - 1
+    if (argc == 0):
+        print(argc, "arguments.")
     else:
-        print(argc, "arguments:")
-    for i, j in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, j))
+        if (argc == 1):
+            print(argc, "argument:")
+        else:
+            print(argc, "arguments:")
+        for i, j in enumerate(argv):
+            if i > 0:
+                print("{:d}: {:s}".format(i, j))
