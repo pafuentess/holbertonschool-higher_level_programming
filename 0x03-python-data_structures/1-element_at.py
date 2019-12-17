fc08337e96b1648f08f0b24dae570e1b1788ec1e
pafuentess <pafuentess@unal.edu.co>
2019-12-17 12:22:46 -0500

    fix1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,6 @@
 
 
 def element_at(my_list, idx):
-    if (idx < 0):
-        return (None)
-    longi = len(my_list)
-    if (idx > longi):
+    if (idx < 0 or (idx > (len(my_list)))):
         return (None)
     return (my_list[idx])
