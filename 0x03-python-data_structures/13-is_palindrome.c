c2ca4811ae3e5994afa38c1c85d99fbe78bb1c8d
pafuentess <pafuentess@unal.edu.co>
2019-12-17 19:17:08 -0500

    fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -39,8 +39,8 @@ int is_palindrome(listint_t **head)
 
 	for (i = 0; i <= longi - 1; i++)
 	{
-		str[i] = *head->n;
-		*head = *head->next;
+		str[i] = (*head)->n;
+		*head = (*head)->next;
 	}
 	i--;
 	for (j = 0 ; j < longi; j++)
