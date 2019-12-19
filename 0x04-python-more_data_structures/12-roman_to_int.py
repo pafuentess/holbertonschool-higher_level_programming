1aa9d23e4eaf75b74effd78bab67e4c738f4afef
pafuentess <pafuentess@unal.edu.co>
2019-12-19 14:14:22 -0500

    condition add

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,8 +4,15 @@
 def roman_to_int(roman_string):
     if (type(roman_string) is not str):
         return (0)
+    if (roman_string == ""):
+        return (0)
     r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     suma = 0
+    check = 0
     for i in roman_string:
-        suma = suma + r[i]
-    return suma
+        check = r.get(i)
+        if check:
+            suma = suma + r[i]
+        else:
+            return (0)
+    return (suma)
