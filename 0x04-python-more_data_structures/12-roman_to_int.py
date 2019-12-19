692751887e03dcfb6c631518f987381b85fdce99
pafuentess <pafuentess@unal.edu.co>
2019-12-19 13:33:23 -0500

    true condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,9 @@
 
 
 def roman_to_int(roman_string):
-    romano = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    suma = 0
-    for i in roman_string:
-        suma = suma + romano[i]
-    return suma
+    if (roman_string):
+        r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        suma = 0
+        for i in roman_string:
+            suma = suma + r[i]
+        return suma
