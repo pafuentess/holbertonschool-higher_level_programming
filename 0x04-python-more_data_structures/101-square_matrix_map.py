122b169fdaa439838ab67d1e1713e452f0b7cc6c
pafuentess <pafuentess@unal.edu.co>
2019-12-19 15:08:37 -0500

    task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map((lambda i: list(map((lambda x: x * x), i))), matrix))
+    return (list(map((lambda i: list(map((lambda x: x * x), i))), matrix)))
