28ae806aaf2dfdb3c51d1a69a7b63a9c3369f0b5
pafuentess <pafuentess@unal.edu.co>
2019-12-19 14:30:05 -0500

    substract condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,25 @@
 
 
 def roman_to_int(roman_string):
+
     if (type(roman_string) is not str):
         return (0)
     if (roman_string == ""):
         return (0)
+
     r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
     suma = 0
+    prev = 0
     check = 0
+
     for i in roman_string:
-        check = r.get(i)
-        if check:
-            suma = suma + r[i]
+        check = r.get(i, 0)
+        if (check > prev):
+            suma = suma - (2 * prev)
+            suma = suma + check
         else:
-            return (0)
+            suma = suma + check
+        prev = check
+
     return (suma)
