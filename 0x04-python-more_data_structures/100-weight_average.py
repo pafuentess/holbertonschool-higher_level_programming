f22c68ae4d5a5925ac1ab8d78c5babc6df8961fe
pafuentess <pafuentess@unal.edu.co>
2019-12-19 14:52:47 -0500

    empty condition

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,12 +2,14 @@
 
 
 def weight_average(my_list=[]):
-    sumap = 0
-    sumad = 0
-    result = 0
-    for tupla in my_list:
-        sumap = sumap + (tupla[0] * tupla[1])
-        sumad = sumad + tupla[1]
+    if (my_list):
+        sumap = 0
+        sumad = 0
+        result = 0
+        for tupla in my_list:
+            sumap = sumap + (tupla[0] * tupla[1])
+            sumad = sumad + tupla[1]
 
-    result = sumap / sumad
-    return (result)
+        result = sumap / sumad
+        return (result)
+    return (0)
