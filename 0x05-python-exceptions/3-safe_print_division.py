41545f2ba7930617d21e0d798b0c249c16edd82a
pafuentess <pafuentess@unal.edu.co>
2020-01-08 12:16:20 -0500

    from 0 to 2

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except(ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return (result)
