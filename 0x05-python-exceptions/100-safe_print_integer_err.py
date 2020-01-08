390256e15d176c10c18ee50e3b762497db2bd587
pafuentess <pafuentess@unal.edu.co>
2020-01-08 14:55:06 -0500

    100 task

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+
+    try:
+        print("{:d}".format(value))
+
+    except (TypeError, ValueError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
+
+    return (True)
