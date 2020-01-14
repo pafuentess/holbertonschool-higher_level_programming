51655eceb308ffac1f20f53fb524e9491a4202d9
pafuentess <pafuentess@unal.edu.co>
2020-01-14 14:58:21 -0500

    all task

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if (type(height) is not int):
+            raise TypeError("height must be an integer")
+        if (height < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if (type(width) is not int):
+            raise TypeError("width must be an integer")
+        if (width < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return (0)
+        return((2*self.__width) + (2*self.height))
+
+    def __str__(self):
+        string = ""
+        if (self.__width == 0 or self.__height == 0):
+            return (string)
+        else:
+            for i in range(self.__height):
+                string = string + ("#" * self.__width)
+                string = string + "\n"
+        return (string[:-1])
+
+    def __repr__(self):
+        return ("Rectangle (%s, %s)" % (self.width, self.height))
+
+    def __del__(self):
+        print("Bye rectangle...")
