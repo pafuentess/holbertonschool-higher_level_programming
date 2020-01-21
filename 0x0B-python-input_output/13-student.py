94ef315a4f5d4d5947bff638ba517e0d414b60d1
pafuentess <pafuentess@unal.edu.co>
2020-01-21 18:47:09 -0500

    task13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" this file contains one class"""
+
+
+class Student:
+
+    """ the student class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        store_values = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                store_values[i] = self.__dict__[i]
+        return (store_values)
+
+    def reload_from_json(self, json):
+        key = json.keys()
+        for i in key:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
