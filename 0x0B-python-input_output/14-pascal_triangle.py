f4252d86c7371c95c14df3478ed0ce29c25fb6ef
pafuentess <pafuentess@unal.edu.co>
2020-01-21 20:02:32 -0500

    task 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+
+    triangle = [[1]]
+    new = []
+    lenght = 0
+    if n <= 0:
+        return (new_row)
+
+    for row in range(0, n-1):
+        lenght = len(triangle[row])
+        for element in range(lenght + 1):
+            if element == 0 or element == lenght:
+                new.append(1)
+            else:
+                new.append(triangle[row][element - 1] + triangle[row][element])
+        copy_row = new[:]
+        triangle.append(copy_row)
+        new.clear()
+    return (triangle)
