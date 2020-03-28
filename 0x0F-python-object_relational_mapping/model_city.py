db52dd650d82b8483c2a24a1276caca1b6e0fa36
pafuentess <pafuentess@unal.edu.co>
2020-03-28 15:59:46 -0500

    task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" class cities """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from model_state import Base
+from sqlalchemy import ForeignKey
+
+
+class City(Base):
+    """ cities """
+    __tablename__ = 'cities'
+    id = Column('id', Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
+    state_id = Column('state_id', Integer, ForeignKey("states.id"),
+                      nullable = False)
