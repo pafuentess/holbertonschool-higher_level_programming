fe08561424bbf43aacbfaa5858ef8d751f21a129
pafuentess <pafuentess@unal.edu.co>
2020-04-09 13:26:37 -0500

    ultima linea

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl -sI "$1" | grep -Po "(Content-Lenght: ).*" 
+curl -sI "$1" | grep -Po "(Content-Lenght: ).*"
+
