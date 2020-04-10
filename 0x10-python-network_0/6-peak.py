5781cb4e3d03cd10464f9e83b47d21963f0f2dfa
pafuentess <pafuentess@unal.edu.co>
2020-04-10 15:44:59 -0500

    tsk 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,5 +5,5 @@
 def find_peak(list_of_integers):
     """ doc """
     if list_of_integers:
-        sorted_list = sorted(list_of_integers)
-        return (sorted_list[-1])
+        list_of_integers.sort()
+        return (list_of_integers[-1])
