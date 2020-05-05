5d4c1913939fbd3082cdf9c0f68e41f762ecce06
pafuentess <pafuentess@unal.edu.co>
2020-05-05 13:55:25 -0500

    fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,8 @@ const length = process.argv.length;
 if (length === 2 || length === 3) {
   console.log(0);
 } else if (length > 3) {
-  const order = process.argv.sort();
-  console.log(order[length - 2]);
+  let order = process.argv.slice(2);
+  order = order.sort();
+  const lenOr = order.length;
+  console.log(order[lenOr - 2]);
 }
