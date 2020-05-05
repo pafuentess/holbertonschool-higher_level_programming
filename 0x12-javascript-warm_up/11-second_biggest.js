9c4c29872933836f9235be359e98fc1a3a97b57a
pafuentess <pafuentess@unal.edu.co>
2020-05-05 18:10:34 -0500

    fix sort cheating sara

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 
+function compareNumbers (a, b) {
+  return a - b;
+}
+
 const length = process.argv.length;
 
 if (length < 4) {
   console.log(0);
 } else {
-  let order = process.argv.slice(2);
-  order = order.sort();
+  const order = process.argv.slice(2);
+  order.sort(compareNumbers);
   const lenOr = order.length;
   console.log(order[lenOr - 2]);
 }
