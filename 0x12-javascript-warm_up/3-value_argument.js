f3a7eea3c51a5eb60105d37970116565b7281aed
pafuentess <pafuentess@unal.edu.co>
2020-05-05 12:41:14 -0500

    remove length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const length = process.argv.length;
-if (length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (length > 2) {
+} else {
   console.log(process.argv[2]);
 }
